15ab67a3033427cc67d2b656e303247bbffa4634
nagaraj-nk <nnagarajece31@gmail.com>
2022-10-08 17:11:52 +0530

    junit- inject mocks

--- src/main/java/com/srjons/DatabaseRepo.java
+++ src/main/java/com/srjons/DatabaseRepo.java
@@ -2,7 +2,7 @@ package com.srjons;
 
 import java.util.List;
 
-public class MockDatabaseRepo {
+public class DatabaseRepo {
 
     public List<String> findItems() {
         return List.of("Hi", "Hello");
